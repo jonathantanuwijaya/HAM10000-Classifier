7fd390c2a706fafb0aeb10b3dd7d8f645200fe5d
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2021-10-16 23:20:11 +0700

    refactor code

--- ConfusionMatrix.py
+++ ConfusionMatrix.py
@@ -1,38 +1,31 @@
 from matplotlib import pyplot as plt
-from sklearn.metrics import precision_score, recall_score, accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 import numpy as np
 
-label = ['mel', 'akiec', 'bcc', 'nv', 'bkl']
+categories = ['mel', 'akiec', 'bcc', 'nv', 'bkl']
 
 
-def plot_confusion_matrix(y_true, y_pred, classes, normalize=True,
-                          title=None,
-                          cmap=plt.cm.Blues):
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=True, title=None, cmap=plt.cm.Blues):
     cm = confusion_matrix(y_true, y_pred)
     cm2 = cm.astype('float') / cm.sum(axis=0)[:, np.newaxis]
     cm2 = cm2 * 100
-    print(cm)
+    # print(cm)
 
     fig, ax = plt.subplots(figsize=(10, 10))
     im = ax.imshow(cm2, interpolation='nearest', cmap=cmap)
     ax.figure.colorbar(im, ax=ax)
     ax.set(xticks=np.arange(cm2.shape[1]),
            yticks=np.arange(cm2.shape[0]),
-           xticklabels=label, yticklabels=label,
+           xticklabels=categories, yticklabels=categories,
            title=title,
-           ylabel='Predicted label',
-           xlabel='True label')
-
-    plt.setp(ax.get_xticklabels(), rotation=0, ha="right",
-             rotation_mode="anchor")
-
+           ylabel='Predicted Tabel',
+           xlabel='True Label')
+    plt.setp(ax.get_xticklabels(), rotation=0, ha='right', rotation_mode='anchor')
     for i in range(cm.shape[0]):
         for j in range(cm.shape[1]):
             z = cm[i, j]
             z = np.around(z)
 
-            ax.text(j, i, format(z),
-                    ha="center", va="center",
-                    color="white" if i == j else "black")
+            ax.text(j, i, format(z), ha='center', va='center', color='white' if i == j else 'black')
     fig.tight_layout()
     return ax
